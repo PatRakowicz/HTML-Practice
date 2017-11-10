3c00f4006cfa3ccc5ddda85a5c425582dfab4415
lividsubset3 <lividsubset3@gmail.com>
2017-11-10 13:07:27 -0700

    Removed Set level
    This was an item that i was trying to do, so you could set the level your self and see how far you can get.

--- Simon-Says/js/script.js
+++ Simon-Says/js/script.js
@@ -186,23 +186,3 @@ $(document).ready(function(){
         
 	});
 });
-
-
-#function {
-
-var looper;
-var degrees = 0;
-function rotateAnimation(el,speed) {
-	var elem = document.getElementById(el);
-	if (navigator.userAgent.match("Chrome")){
-		elem.style.WebkitTransform = "rotate("+degrees+"deg)";
-    } else {
-		elem.style.transform = "rotate("+degrees+"deg)";
-	}
-	looper = setTimeout$('rotateAnimation(\''+el+'\','+speed+')',speed);
-	degrees++;
-	if (degrees > 359){
-		degrees = 1;
-	}
-	document.getElementById$("back").innerHTML = "rotate("+degrees+"deg)";
-};
